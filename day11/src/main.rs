76f37bc1253922d17bc2608e9f10f236eb5c65bd
olig331 <ohg3@hotmail.co.uk>
2024-12-11 16:00:54 +0000

    Clean up more

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -24,7 +24,6 @@ fn parse_input(path: &str) -> BTreeMap<u64, u64> {
     map
 }
 
-#[rustfmt::skip]
 fn condition_one(start_map: &mut BTreeMap<u64, u64>, item: u64, count: u64) {
     if let Some(x) = start_map.get_mut(&item) {
         *x -= count;
@@ -32,11 +31,9 @@ fn condition_one(start_map: &mut BTreeMap<u64, u64>, item: u64, count: u64) {
     }
 }
 
-#[rustfmt::skip]
 fn condition_two(start_map: &mut BTreeMap<u64, u64>, item: u64, count: u64) {
     let s = item.to_string();
     let (first, second) = s.split_at(s.len() / 2);
-
     for half in [first, second] {
         if let Ok(num) = half.parse::<u64>() {
             *start_map.entry(num).or_insert(0) += count;
@@ -45,7 +42,6 @@ fn condition_two(start_map: &mut BTreeMap<u64, u64>, item: u64, count: u64) {
     *start_map.get_mut(&item).expect("...") -= count;
 }
 
-#[rustfmt::skip]
 fn condition_three(start_map: &mut BTreeMap<u64, u64>, item: u64, count: u64) {
     if let Some(current) = start_map.get_mut(&item) {
         *current -= count;
@@ -53,33 +49,23 @@ fn condition_three(start_map: &mut BTreeMap<u64, u64>, item: u64, count: u64) {
     }
 }
 
+#[rustfmt::skip]
 fn solve(map: BTreeMap<u64, u64>, range: usize) -> u64 {
     let mut start_map = map.clone();
     for _ in 0..range {
         for (item, count) in start_map.clone().iter() {
-            if *count == 0 {
-                continue;
+            if *count == 0 { 
+                continue 
+            };
+            
+            match item {
+                item if *item == 0 => condition_one(&mut start_map, *item, *count),                
+                item if item.to_string().len() % 2 == 0 => condition_two(&mut start_map, *item, *count),
+                _ => condition_three(&mut start_map, *item, *count),
             }
-
-            if *item == 0 {
-                condition_one(&mut start_map, *item, *count);
-                continue;
-            }
-
-            if item.to_string().len() % 2 == 0 {
-                condition_two(&mut start_map, *item, *count);
-                continue;
-            }
-
-            condition_three(&mut start_map, *item, *count);
         }
     }
-
-    let mut sum = 0 as u64;
-    for (_, val) in start_map.iter() {
-        sum += val;
-    }
-    sum
+    start_map.values().sum()
 }
 
 #[cfg(test)]
@@ -89,6 +75,6 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(solve(parse_input(TEST_INPUT_PATH), 25), 55312);
+        assert_eq!(solve(parse_input(TEST_INPUT_PATH), 25), 55_312);
     }
 }
